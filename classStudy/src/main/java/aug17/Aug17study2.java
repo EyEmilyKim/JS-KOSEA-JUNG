e7838ff3da935563339b96bec6dd1a43d2ae6c9e
EyEmilyKim kosea1 <myue4555@gmail.com>
2022-08-18 09:40:05 +0900

    [UPDATE] 코바야시, 타나까 높이뛰기 추가

--- classStudy/src/main/java/aug17/Aug17study2.java
+++ classStudy/src/main/java/aug17/Aug17study2.java
@@ -4,18 +4,29 @@ package aug17;
 //고바야시와 다나까의 기록을 출력한다.
 //스즈키가 고바야시와 다나까의 기록을 비교해서 승패를 판단한다.
 //클래스를 찾는 작업(SRP)
+//((과제))
+//고바야시와 다나까가 높이뛰기를 했습니다.
+//사토가 고바야시와 다나까의 기록을 비교해서 높이뛰기 승패를 판단합니다.
 class Kobayashi {
 	double record = 0;
+	double recordH = 0;
 	Kobayashi(){}
 	void jump() {
 		record = (int)((Math.random()*3000)+1)/10.0;
 	}
+	void jumpHigh() {
+		recordH = (int)(Math.random()*4000+1)/10.0;
+	}
 }
 class Tanaka {
 	double record = 0;
+	double recordH = 0;
 	Tanaka(){}
 	void jump() {
 		record = (int)((Math.random()*3000)+1)/10.0;
+	}
+	void jumpHigh() {
+		recordH = (int)(Math.random()*4000+1)/10.0;
 	}
 }
 class Suzuki {
@@ -30,11 +41,17 @@ class Suzuki {
 		}else if(k.record < t.record){ System.out.println("Tanaka 승");
 		}else { System.out.println("무승부");}
 	}
+	void winnerH(double KrecH, double TrecH) {
+		if(KrecH > TrecH) { System.out.println("Kobayashi 승");
+		}else if(KrecH < TrecH) { System.out.println("Tanaka 승");
+		}else { System.out.println("무승부"); }
+	}
 }
 public class Aug17study2 {
 	public static void main(String[] args) {
 		Kobayashi k = new Kobayashi();
 		Tanaka t = new Tanaka();
+		System.out.println("--멀리뛰기--");
 		System.out.println("뛰기 전 K: "+k.record);
 		System.out.println("뛰기 전 T: "+t.record);
 		k.jump();
@@ -44,6 +61,15 @@ public class Aug17study2 {
 		Suzuki s = new Suzuki();
 		s.winner(k.record, t.record);
 		s.winner2(k, t);
+		
+		System.out.println("--높이뛰기--");
+		System.out.println("뛰기 전 K: "+k.recordH);
+		System.out.println("뛰기 전 T: "+t.recordH);
+		k.jumpHigh();
+		t.jumpHigh();
+		System.out.println("뛴 후 K: "+k.recordH);
+		System.out.println("뛴 후 T: "+t.recordH);
+		s.winnerH(k.recordH, t.recordH);
 	}
 
 }
