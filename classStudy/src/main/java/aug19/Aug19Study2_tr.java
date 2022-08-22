4764512e8ef2ef0dd75d0882a9284445e8f11acc
EyEmilyKim kosea3 <myue4555@gmail.com>
2022-08-22 16:42:17 +0900

    [UPDATE] 2022.08.22월_ 자바컴퍼니 사장 월급계산

--- classStudy/src/main/java/aug19/Aug19Study2_tr.java
+++ classStudy/src/main/java/aug19/Aug19Study2_tr.java
@@ -8,6 +8,21 @@ package aug19;
 //개발부 과장이 개발부 부하 직원의 월급의 합과 평균을 출력합니다.
 //2.자바 주식회사에 사장이 있습니다. 
 //사장이 모든 부하 직원의 월급의 합과 평균을 출력합니다.
+class CEO {
+	CEO(){}
+	void show(JavaCompany jc) {
+		int sum = jc.sd.abe.salary+jc.sd.ogawa.salary+jc.sw.ak.salary+jc.sw.f.salary+jc.sw.n.salary;
+		double avg = sum / 5.0;
+		System.out.println("--나 사장이다. 자바컴퍼니\n합:"+sum+", 평균:"+avg);
+	}
+	void show(SalesDept sd, SWDept sw) {//모든 직원의 월급의 합과 평균을 출력
+		int sum = sd.abe.salary+sd.ogawa.salary+sw.ak.salary+sw.f.salary+sw.n.salary;
+		double avg = sum / 5.0;
+		System.out.println("--나 사장이다. 영업부+개발부\n합:"+sum+", 평균:"+avg);
+	}
+
+}
+
 class SWManager {
 	public SWManager() {}
 	void sumNavg(SWDept d) {
@@ -53,20 +68,26 @@ class SalesDept{
 	Abe abe; Ogawa ogawa; SalesManager manager;
 }
 class JavaCompany {
-	JavaCompany(){ sd = new SalesDept(); sw = new SWDept(); }
-	SalesDept sd; SWDept sw;
+	JavaCompany(){ sd = new SalesDept(); sw = new SWDept(); ceo = new CEO(); }
+	SalesDept sd; SWDept sw; CEO ceo;
 }
 
 public class Aug19Study2_tr {
 	public static void main(String[] args) {
 		JavaCompany jc = new JavaCompany();
+		//자바회사의 영업부 과장이 합과 평균 출력.
 		System.out.println("아베:"+jc.sd.abe.salary);
 		System.out.println("오가와:"+jc.sd.ogawa.salary);
 		jc.sd.manager.sumNavg(jc.sd);
+		//자바회사의 개발부 과장이 합과 평균 출력.
 		System.out.println("나카지마:"+jc.sw.n.salary);
 		System.out.println("후지타:"+jc.sw.f.salary);
 		System.out.println("아오키:"+jc.sw.ak.salary);
 		jc.sw.manager.sumNavg(jc.sw);
+		//사장이 영업부와 개발부 직원 월급의 합과 평균 출력.
+		jc.ceo.show(jc.sd, jc.sw);
+		//사장이 자바컴퍼니 직원 월급의 합과 평균 출력.
+		jc.ceo.show(jc);
 	}
 
 }
