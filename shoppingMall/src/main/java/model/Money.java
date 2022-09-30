3fac442f565a847f76aedd8571d3e27a3669ce73
EyEmilyKim kosea3 <myue4555@gmail.com>
2022-09-30 17:39:41 +0900

    [INITIAL] 2022.09.30금_ 회원별 매출 조회

---
+++ shoppingMall/src/main/java/model/Money.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Money {
+	private Integer custno;
+	private String custname;
+	private String grade;
+	private Integer total;
+	public Integer getCustno() {
+		return custno;
+	}
+	public void setCustno(Integer custno) {
+		this.custno = custno;
+	}
+	public String getCustname() {
+		return custname;
+	}
+	public void setCustname(String custname) {
+		this.custname = custname;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public Integer getTotal() {
+		return total;
+	}
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+	
+}
