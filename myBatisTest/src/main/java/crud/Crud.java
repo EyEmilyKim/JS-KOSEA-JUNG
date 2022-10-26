6987996f4ada284904364e469066538571c5fdd0
EyEmilyKim kosea3 <myue4555@gmail.com>
2022-10-26 11:43:49 +0900

    [UPDATE] 2022.10.26수_ myBatis 학습

--- myBatisTest/src/main/java/crud/Crud.java
+++ myBatisTest/src/main/java/crud/Crud.java
@@ -1,6 +1,7 @@
 package crud;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -12,11 +13,108 @@ import model.Test;
 public class Crud {
 //매퍼를 호출하는 객체(SqlSession)를 생성한다.
 //SqlSession을 사용해서 매퍼 안의 쿼리를 호출하는 메소드를 작성한다.
+	private final String mapperName = "mapper.test";
 	
+	public ArrayList<Test> getAll(){
+		SqlSession ss = this.getSession();
+		ArrayList<Test> result = null; //조회결과 저장용 변수
+		try {
+			String name = mapperName+".getAll";
+			result = (ArrayList)ss.selectList(name);
+		}finally {
+			ss.close();
+		}
+		return result;
+	}
+	
+	//번호와 이름으로 번호, 이름, 주소, 날짜 찾는 메서드
+	public Test getAllByNoName(Test test) {
+		SqlSession ss = this.getSession();
+		Test result = null; //조회결과를 저장할 변수
+		try {
+			String name = this.mapperName+".getAllByNoName";
+			result = ss.selectOne(name, test);
+		}finally {
+			ss.close();
+		}
+		return result;
+	}
+	
+	//번호로 이름, 주소, 날짜 찾는 메서드
+	public Test getNameAddrDate(Integer no) {
+		SqlSession ss = this.getSession();
+		Test test = null; //조회결과를 저장할 변수 선언
+		try {
+			String name = this.mapperName+".getNameAddrDate";
+			test = ss.selectOne(name, no);
+		}finally {
+			ss.close();
+		}
+		return test;
+	}
+	
+	//번호로 날짜 조회하는 메서드
+	public String getDate(Integer no) {
+		SqlSession ss = this.getSession();
+		String date = null;
+		try {
+			String name = this.mapperName+".getDate";
+			date = ss.selectOne(name, no);
+		}finally {
+			ss.close();
+		}
+		return date;
+	}
+	
+	//번호로 이름 조회하는 메서드
+	public String getName(Integer no) {
+		SqlSession ss = this.getSession();
+		String name = null; //조회결과를 저장할 변수 선언
+		try {
+			String sql = this.mapperName+".getName";
+			name = ss.selectOne(sql, no);
+			//조회결과가 1건인 경우 사용하는 메서드 : selectOne()		
+		}finally {
+			ss.close();
+		}
+		return name;
+	}
+	
+	//수정하는 메서드
+	public Integer updateData(Test test) {
+		SqlSession ss = this.getSession();
+		Integer result = -1;
+		try {
+			String name = this.mapperName+".updateData";
+			result = ss.update(name, test);
+			if(result > 0) ss.commit();
+			else ss.rollback();
+		}finally {
+			ss.close();
+		}
+		return result;
+	}
+	
+	//삭제하는 메서드
+	public Integer deleteData(Integer no) {
+		SqlSession ss = this.getSession();
+		Integer result = -1;
+		try {
+			String name = mapperName+".deleteData"; //매퍼이름.쿼리이름
+			result = ss.delete(name, no);
+			if(result > 0) ss.commit();
+			else ss.rollback();
+		}finally {
+			ss.close(); //세션 반납
+		}
+		return result;
+	}
+	
+	//삽입하는 메서드
 	public Integer putData(Test test){
 		SqlSession ss = getSession();
 		Integer result = -1;
-		String name = "mapper.test.putDate"; //매퍼이름.쿼리이름
+		String name = mapperName+".putDate"; //매퍼이름.쿼리이름(매퍼의 namespace, 쿼리의 id)
 		try {
 			result = ss.insert(name, test); //매핑할 쿼리, 매개변수
 			if(result > 0) ss.commit(); //양수 나오면 성공 -> 커밋
@@ -27,6 +125,7 @@ public class Crud {
 		return result;
 	}
 	
+	//SqlSession 만드는 메서드
 	private SqlSession getSession() {
 		String config = "mybatisConfig.xml"; //환경설정파일명
 		InputStream is = null;
--- myBatisTest/src/main/java/main/Main.java
+++ myBatisTest/src/main/java/main/Main.java
@@ -1,21 +1,92 @@
 package main;
 
+import java.util.ArrayList;
+
 import crud.Crud;
 import model.Test;
 
 public class Main {
 
 	public static void main(String[] args) {
-		//삽입 작업
-		Test test = new Test();
-		test.setNo(2);
-		test.setName("나주작");
-		test.setAddr("서울");
-		test.setReg_date("2022-09-25");
 		Crud crud = new Crud();
-		int r = crud.putData(test);
-		if(r > 0) System.out.println("삽입 성공!");
-		else System.out.println("삽입 실패");
+
+		//삽입 작업
+//		Test test = new Test();
+//		test.setNo(5);
+//		test.setName("하현무");
+//		test.setAddr("서울");
+//		test.setReg_date("2022-09-25");
+//		int r = crud.putData(test);
+//		if(r > 0) System.out.println("삽입 성공!");
+//		else System.out.println("삽입 실패");
+		
+		//삭제 작업
+//		int r = crud.deleteData(1);
+//		if(r > 0) System.out.println("삭제 성공!");
+//		else System.out.println("삭제 실패ㅠㅠ");
+		
+		//변경 작업
+//		Test test = new Test();
+//		test.setNo(2);
+//		test.setName("김해치");
+//		test.setAddr("서울시 경복궁");
+//		test.setReg_date("2022-10-26");
+//		int r = crud.updateData(test);
+//		if(r > 0) System.out.println("수정 성공");
+//		else System.out.println("수정 실패");
+		
+		//조회 (번호로 이름)
+//		String name = crud.getName(2);
+//		System.out.println("조회결과 : "+name);
+		
+		//조회 (번호로 날짜)
+//		String date = crud.getDate(2);
+//		System.out.println("조회결과 : "+date);
+		
+		//조회 (번호로 이름, 주소, 날짜)
+//		Integer no = 2;
+//		Test result = crud.getNameAddrDate(no);
+//		System.out.println("---getNameAddrDate(no)---");
+//		if(result == null) {
+//			System.out.println("조회결과가 존재하지 않습니다.");
+//		}else {
+//			System.out.println("no : "+no);
+//			System.out.println("이름 : "+result.getName());
+//			System.out.println("주소 : "+result.getAddr());
+//			System.out.println("날짜 : "+result.getReg_date());
+//		}
+		
+		//조회 (번호와 이름으로/ 번호,이름,주소,날짜를 검색)
+//		Integer num = 2;
+//		String name = "김해치";		
+//		Test test = new Test();
+//		test.setNo(num);
+//		test.setName(name);
+//		Test result = crud.getAllByNoName(test);
+//		System.out.println("---getAllByNoName(test)---");
+//		System.out.println("param num = "+num);
+//		System.out.println("param name = "+name);
+//		if(result == null) {
+//			System.out.println("조회결과가 존재하지 않습니다.");
+//		}else {
+//			System.out.println("번호 : "+result.getNo());
+//			System.out.println("이름 : "+result.getName());
+//			System.out.println("주소 : "+result.getAddr());
+//			System.out.println("날짜 : "+result.getReg_date());
+//		}
+		
+		//전체 조회
+		ArrayList al = crud.getAll();
+		System.out.println("---getAll()---");
+		for(int i=0; i<al.size(); i++) {
+			Test test = (Test)al.get(i);
+			System.out.println("번호 : "+test.getNo());
+			System.out.println("이름 : "+test.getName());
+			System.out.println("주소 : "+test.getAddr());
+			System.out.println("날짜 : "+test.getReg_date());
+			System.out.println();
+		}
+		System.out.println("---getAll() end---");
 	}
 
 }
