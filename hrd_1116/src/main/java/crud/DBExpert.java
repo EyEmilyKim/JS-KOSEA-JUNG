3c739c570f732f1c53815776efb665e75fa20c61
EyEmilyKim kosea3 <myue4555@gmail.com>
2022-11-16 17:32:43 +0900

    [INIT/UPD] 2022.11.16수_ hrd1116 쇼핑몰 예제 : 회원목록

--- hrd_1116/src/main/java/crud/DBExpert.java
+++ hrd_1116/src/main/java/crud/DBExpert.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import model.Member;
 
@@ -16,6 +17,43 @@ public class DBExpert {
 	private Connection conn;
 	private PreparedStatement pstmt;
 	private ResultSet rs;
+	
+	//전체 회원 검색
+	public ArrayList<Member> getMembers() {
+		String select = "select custno, custname, phone, address, "
+				+ "to_char(joindate, 'yyyy/mm/dd'), grade, city "
+				+ "from member_tbl_02";
+		ArrayList<Member> list = new ArrayList<Member>();
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url,id,pw);
+			pstmt = conn.prepareStatement(select);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				Member mem = new Member();
+				mem.setId(rs.getInt(1));
+				mem.setName(rs.getString(2));
+				mem.setTel(rs.getString(3));
+				mem.setAddr(rs.getString(4));
+				mem.setDate(rs.getString(5));
+				String grade = rs.getString(6);
+				switch(grade) {
+				case "A" : mem.setGrade("VIP"); break; 
+				case "B" : mem.setGrade("일반"); break; 
+				case "C" : mem.setGrade("직원"); break; 
+				}
+				mem.setCity(rs.getString(7));
+				list.add(mem);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try { rs.close(); pstmt.close(); conn.close(); }
+			catch(Exception e) {}
+		}
+		return list;
+		
+	}
 	
 	//회원 삽입
 	public boolean putMember(Member mem) {
---
+++ hrd_1116/src/main/webapp/userList.jsp
@@ -0,0 +1,49 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="model.*, java.util.*" %>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<div align="center">
+<header>
+<h2>쇼핑몰 회원관리 ver1.0</h2>
+</header>
+<menu>
+<%@ include file="menu_header.jsp" %>
+</menu>
+<section>
+
+<h2 align="center">회원목록 조회/수정</h2>
+<br/>
+
+<table border="1">
+<tr><th>회원번호</th><th>회원성명</th><th>전화번호</th><th>주소</th>
+	<th>가입일자</th><th>고객등급</th><th>거주지역</th></tr>
+<%
+	ArrayList<Member> list = (ArrayList) request.getAttribute("LIST");
+	for(Member mem : list){
+%>
+<tr><td><%= mem.getId() %></td>
+	<td><%= mem.getName() %></td>
+	<td><%= mem.getTel() %></td>
+	<td><%= mem.getAddr() %></td>
+	<td><%= mem.getDate() %></td>
+	<td><%= mem.getGrade() %></td>
+	<td><%= mem.getCity() %></td></tr>
+<%		
+	}
+%>
+</table>
+
+<br/>
+</section>
+<footer>
+<h4>HRDKorea Copyright 2022. 과정평가형 시스템 작성</h4>
+</footer>
+</div>
+</body>
+</html>
